a3f69ed103ba006fc6a502cca4a83e4a08a0e9db
Luis Bazán <me@luisbazan.dev>
2022-04-14 09:39:44 -0500

    whatsapp utils functions: getLocalVideo, getLocalImage and resolveChannelType

--- src/whatsapp/Util.js
+++ src/whatsapp/Util.js
@@ -1,11 +1,36 @@
-const { ChatTypes } = require('whatsapp-web.js')
-const formatChannel = (channel) => {
-  const resp = {
-    id: channel.id,
-    type: undefined,
-    name: undefined,
+const { ChatTypes, MessageMedia } = require('whatsapp-web.js')
+const fs = require('fs')
+
+/**
+ * 
+ * @param {Chat} chat 
+ * @returns
+ */
+const resolveChannelType = (chat) => {
+  if(chat.isGroup) {
+    return "group";
+  } else if(chat.isReadOnly) {
+    return "readonly";
   }
-  if(channel.isGroup) {
-    return `${channel.title} (${channel.id})`;
-  }
+  return "private";
+}
+
+/**
+ * 
+ * @param {String} path Path of the project root
+ * @returns MessageMedia
+ */
+const getLocalImage = (path) => {
+  const file = fs.readFileSync(path, 'base64');
+  return new MessageMedia(file, 'image/png');
+}
+
+/**
+ * 
+ * @param {String} path Path of the project root
+ * @returns MessageMedia
+ */
+const getLocalVideo = (path) => {
+  const file = fs.readFileSync(path, 'base64');
+  return new MessageMedia(file, 'video/mp4');
 }
